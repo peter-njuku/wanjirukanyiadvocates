6ec3079e1e375831bc39a34ff7fd3fe424f46ea6
peter-njuku <njuku2005@gmail.com>
2025-08-21 17:19:21 +0300

    Contact

--- next.config.mjs
+++ next.config.mjs
@@ -16,11 +16,11 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self';"
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://docs.google.com; style-src 'self' 'unsafe-inline' https://docs.google.com; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https://docs.google.com; frame-src 'self' https://docs.google.com https://forms.gle;"
           },
           {
             key: 'X-Frame-Options',
-            value: 'DENY'
+            value: 'ALLOW-FROM https://docs.google.com'
           },
           {
             key: 'X-Content-Type-Options',
